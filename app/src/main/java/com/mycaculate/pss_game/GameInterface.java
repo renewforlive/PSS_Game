92ea84ca47bdb87043792d5ed1125b2a78f5fcd2
renewforlive <renewforlive1@gmail.com>
2018-06-28 11:06:50 +0800

    20180628 11:06 寫了部分程式

--- app/src/main/java/com/mycaculate/pss_game/GameInterface.java
+++ app/src/main/java/com/mycaculate/pss_game/GameInterface.java
@@ -1,13 +1,36 @@
 package com.mycaculate.pss_game;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class GameInterface extends AppCompatActivity {
+public class GameInterface extends AppCompatActivity implements View.OnClickListener{
+    ImageButton btn_paper,btn_sci,btn_stone;
+    Button btn_rdn;
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_interface);
+
+        btn_sci = findViewById(R.id.btn_scissor);
+        btn_stone = findViewById(R.id.btn_stone);
+        btn_paper = findViewById(R.id.btn_paper);
+        btn_sci.setOnClickListener(this);
+        btn_stone.setOnClickListener(this);
+        btn_paper.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        final View view = inflater.inflate(R.layout.fight_area,null);
+        builder = new AlertDialog.Builder(this);
+
     }
 }
